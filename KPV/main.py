0ddb3bb4b4688a5ac03aae5bd534148a78464513
Afsaneh Mastouri <67098281+Afsaneh-Mastouri@users.noreply.github.com>
2022-07-31 12:37:13 +0100

    Update main.py

--- KPV/main.py
+++ KPV/main.py
@@ -8,8 +8,6 @@ Created on Mon Jan 18 15:38:21 2021
 """
 import os,sys,datetime
 import time
-from pathlib import Path
-from typing import Dict, Any, Iterator, Tuple
 import numpy as np 
 import pandas as pd
 import operator
@@ -28,40 +26,25 @@ import jax.scipy.linalg as jsla
 from scipy.stats import binned_statistic
 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 import inspect
 import glob
 import time
 
 
-path='/Users/afsaneh/Desktop/Final/for_Github'
-
-#current_path=os.path.abspath(os.path.dirname(path))
-#sys.path.append(os.path.join(current_path,'..','utils'))
 
 from utils import *
 from cal_alpha import cal_alpha_opt_post
 
-#% generate random state
-#extra 2keys for choosing train_test data subsets
+
 seed1=547610
 seed2=240650
 seed3=35400
 seed4=1872304
 num_var=6
-sd_lst= [5949, 7422, 4388, 2807, 5654, 5518, 1816, 1102, 9886, 1656, 4379,
-       2029, 8455, 4987, 4259, 2533, 9783, 7987, 1009, 2297] #np.random.choice(10000,20)
+sd_lst= [1009,1102,1656]#5949, 7422, 4388, 2807, 5654, 5518, 1816, 1102, 9886, 1656, 4379,2029, 8455, 4987, 4259, 2533, 9783, 7987, 1009, 2297] 
 
-'''
-key1 = random.PRNGKey(seed1)
-key1, *subkeys1 = random.split(key1, num_var+1)
 
-# extra terms for ACE
-key2 = random.PRNGKey(seed2)
-key2, *subkeys2 = random.split(key2, num_var+1+2*5)
-
-'''
 # verbos 200
 key3 = random.PRNGKey(seed3)
 key3, *subkeys3 = random.split(key3, 2)
@@ -74,63 +57,22 @@ key4, *subkeys4 = random.split(key4, 1000*num_var)
 
 ewh_dict={}
 
-#mix_w=[0.8, 0.2]
 
-'''
- 
-for sd in sd_lst:#[:1]:
-        #print(sd)
-        #data = np.load('main_seed%s_std.npz' %sd)
-        data=np.load('/Users/afsaneh/Documents/UCL/Thesis/KPV/AG/Final/Final/main_seed%s_std.npz' %sd)
-        #do_cal = np.load('do_A_seed%s_std.npz' %sd)
-        do_cal=np.load('/Users/afsaneh/Documents/UCL/Thesis/KPV/AG/Final/Final/do_A_seed%s_std.npz' %sd)
-        keep_x=False
+
+
+do_cal=np.load('/Users/afsaneh/Downloads/True_Caual_Effect.npz')
+
+# if x={}: keep_x=False, otherwise keep_x=True
+keep_x=True
         
-        #samp_size=[500,1000,1500, 2000]
-        #samp_size=[100,200,300, 500]#,800,1000,1500,2000]
-            
-        '''
-do_cal=np.load('/Users/afsaneh/Documents/UCL/TKPVF/edu/IM/do_A_edu_IM_80_seed100.npz')
-keep_x=False
-for f in glob.glob('/Users/afsaneh/Documents/UCL/TKPVF/edu/IM/main_edu_IM*.npz'):
-        
-        
-        
-        
-        #print(f)
+# if variables need to be standardised (to make sure the average of y=f(x)=0 isin RKHS
+preprocess_var=True
+       
+for f in glob.glob('/Users/afsaneh/Downloads/Data_Sample/*.npz'):
+
         data = np.load(f)    
         
-        
-        data_dict={}
-        for i in data.files:
-            data_dict[i]= data[i]
-        
-        lst_train= ['train_y', 'train_a',  'train_z', 'train_w', 'train_u']
-        lst_test= ['test_y', 'test_a',  'test_z', 'test_w', 'test_u']
-        
-        train =[]
-        test= []
-        
-        for i in  data_dict.keys():
-            if i in lst_train:             
-                    for k in range(data_dict[i].shape[1]):
-                        train.append(data_dict[i][:,k])
-            if i in lst_test:             
-                    for k in range(data_dict[i].shape[1]):
-                        test.append(data_dict[i][:,k])
-     
-        
-        
-        
-        
-        
-        keep_x=False
-        preprocess_var=True
-        
-        
-        
-        start_time = time.time()
-        
+         
         def range_var(arr):
                try:
                    ran_var=arr.shape[1]
@@ -166,11 +108,11 @@ for f in glob.glob('/Users/afsaneh/Documents/UCL/TKPVF/edu/IM/main_edu_IM*.npz')
             return X,X2, lst_x
         
            
-        A = data['train_a']#[:n])
-        Y = data['train_y']#[:n]
-        U = data['train_u']#[:n] #ndarray
-        Z = data['train_z']#[:n]
-        W = data['train_w']#[:n]
+        A = data['train_a']
+        Y = data['train_y']
+        U = data['train_u']
+        Z = data['train_z']
+        W = data['train_w']
         X =  x_inclusion(data, keep_x)[0]
         A2= data['test_a']
         Y2= data['test_y']
@@ -214,24 +156,24 @@ for f in glob.glob('/Users/afsaneh/Documents/UCL/TKPVF/edu/IM/main_edu_IM*.npz')
         if preprocess_var:
             O_train_val, Std_Scale=standardise(O_train_val)
             O_test=standardise(O_test)[0]
-            do_A_s = (do_A-A.mean())/A.std()#[0].inverse_transform(do_A)
-            #gt_EY_do_A = (gt_EY_do_A-Y.mean())/Y.std()#standardise(Y)[1].inverse_transform(gt_EY_do_A)
+            do_A_s = (do_A-A.mean())/A.std()
             
-         #plt.scatter((do_A-A.mean())/A.std(),gt_EY_do_A),plt.scatter(standardise(A)[0],standardise(Y)[0])
+            
+         
         n_test=O_test.shape[0]
         n_val=int(O_test.shape[0])
 
-        #print("--- %s seconds ---" % (time.time() - start_time))   
-
         
-        samp_size=[3000]#[500]#500,1000]#,2000]
+        
+        samp_size=[500]#[1000,2000,3000]
         c=-1
         results={}
-        scale_max=1.5
-        scale_min=1.5
         
-        l_yw_min=.01
-        l_w_min=.001
+        scale_max=1.5
+        scale_min=.5
+        
+        l_yw_min=.001
+        l_w_min=.0001
         optimise_l_yw=False
         optimise_l_w=True
         
@@ -239,14 +181,14 @@ for f in glob.glob('/Users/afsaneh/Documents/UCL/TKPVF/edu/IM/main_edu_IM*.npz')
         for num in range(len(samp_size)):
             ewh_dict_df=pd.DataFrame()
             for key in subkeys3:
-                #for scale in np.linspace(1,5,10):
+                
                     
                     params_h=[]
                     c=c+1
                     n_train=samp_size[num] 
 
-                    m1_train=n_train#int(n_train*0.4)## I have to change it back to 50%
-                    m2_train=n_train#-m1_train
+                    m1_train=n_train#int(n_train*0.4)## change it to any ratio of training 
+                    m2_train=n_train#-m1_train ## based on above
                     
                      
                     
@@ -257,7 +199,7 @@ for f in glob.glob('/Users/afsaneh/Documents/UCL/TKPVF/edu/IM/main_edu_IM*.npz')
                     O_test_df.columns=lst_O
                     
 
-                    
+                    # for pairplot
                     
                     '''
                     sns.pairplot(O_train_val_df)        
@@ -281,7 +223,7 @@ for f in glob.glob('/Users/afsaneh/Documents/UCL/TKPVF/edu/IM/main_edu_IM*.npz')
                                                    lst_z=lst_z,lst_w=lst_w, lst_y=lst_y, scale_mx=scale_max,
                                                    scale_mn=scale_min,int_lst=int_lst,optimise_l_yw=optimise_l_yw, optimise_l_w=optimise_l_w, l_yw_min=l_yw_min,l_w_min=l_w_min)
                  
-                    #print("--- %s seconds ---" % (time.time() - start_time)) 
+                    
                     
                     Ew_h=[]
                     sampl_w=val_samp[lst_w]
@@ -306,14 +248,14 @@ for f in glob.glob('/Users/afsaneh/Documents/UCL/TKPVF/edu/IM/main_edu_IM*.npz')
                     plt.title("l= {l}, MAE_c={b:.3f}".format(l=lambda_dict, b=mse_c))
                     plt.legend(loc='upper left');
                     
-                    plt.savefig('IM_seed'+str(f[-7:])+'_Size'+str(samp_size[num])+'_scale'+str(scale_max)+'.png')#+str(c)+'.png')
+                    plt.savefig('seed'+str(f[-12:-8])+'_Size'+str(samp_size[num])+'_scale'+str(scale_max)+'.png')
                     plt.show()
                     plt.close()
                     
-                    print (f[-7:-4],lambda_dict,mse_c)
+                    print (f[12:-8],lambda_dict,mse_c)
                     
-                    ewh_dict[f[-7:-4],samp_size[num]]=Ew_Haw
+                    ewh_dict[f[12:-8],samp_size[num]]=Ew_Haw
        
         
         
-pickle.dump(ewh_dict,open('results_IM_seed{sb}_size{size}_{date}.p'.format(sb=f[-7:-4],  size=3000 , date=time.strftime("%Y%m%d_%H%M")),'wb'))    
+pickle.dump(ewh_dict,open('results_size{size}_{date}.p'.format(sb=f[-12:-8],  size=samp_size[num] , date=time.strftime("%Y%m%d_%H%M")),'wb'))       
